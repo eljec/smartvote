5d991012c9ed40279c58a459fc4da484f373aa3b
juliocastillo.sistemas@gmail.com <juliocastillo.sistemas@gmail.com@4c492c8d-0530-6077-2cbe-3ff0212ddd7c>
2012-12-17 18:36:52 +0000

    Nueva version.

---
+++ trunk/tv/SmartVote_2/app/scenes/Scene1.js
@@ -0,0 +1,110 @@
+function SceneScene1(options) {
+	this.options = options;
+	this.btnIniciar;
+	this.configuracion;
+	
+
+}
+
+SceneScene1.prototype.initialize = function () {
+	alert("SceneScene1.initialize()");
+	
+	// Se llama cuando se Crea por Primera Vez--Nada Mas  
+		
+	$('#lbTituloSistema').sfLabel({text:'Smart Vote', width:'320px'});
+	$('#B_CargaProgramas').sfButton({text:'Iniciar', width:'109px'});
+	$('#btnTest').sfButton({text:'Ayuda', width:'122px'});
+	$('#logo').sfImage({src:'images/logoTV.png'});
+	$('#helpBar').sfKeyHelp({'LEFTRIGHT':'Moverse entre botones','ENTER':'Enter','return':'Rregresar al Hub'});
+
+	$('#popUp_regresar').sfPopup({text:'¿ Seguro desea salir de la aplicación y regresar al Hub ?', num:'2', callback:function(data){
+		if(data)
+		{
+			$.sf.exit(false);
+		}	
+	}});	
+}
+
+SceneScene1.prototype.handleShow = function () {
+	alert("SceneScene1.handleShow()");
+	
+	// Se llama cuando se hace Foco, a als cosas que pongo o defino aca se les ejecuta su metodo show.
+}
+
+SceneScene1.prototype.handleHide = function () {
+	alert("SceneScene1.handleHide()");
+	// this function will be called when the scene manager hide this scene  
+}
+
+SceneScene1.prototype.handleFocus = function () {
+	alert("SceneScene1.handleFocus()");
+	// this function will be called when the scene manager focus this scene
+	
+	this.btnIniciar = true;
+	$('#B_CargaProgramas').sfButton('focus');
+	$('#btnTest').sfButton('blur');
+	
+	$('#btnTest').sfButton({text:'Ayuda'});
+	
+}
+
+SceneScene1.prototype.handleBlur = function () {
+	alert("SceneScene1.handleBlur()");
+	// this function will be called when the scene manager move focus to another scene from this scene
+}
+
+SceneScene1.prototype.handleKeyDown = function (keyCode) {
+	alert("SceneScene1.handleKeyDown(" + keyCode + ")");
+	// TODO : write an key event handler when this scene get focued
+	switch (keyCode) {
+	
+		case $.sfKey.LEFT:
+			
+			if(!this.btnIniciar)
+			{
+				$('#B_CargaProgramas').sfButton('focus');
+				$('#btnTest').sfButton('blur');
+				this.btnIniciar = true;
+			}
+
+			break;
+		case $.sfKey.RIGHT:
+			
+			if(this.btnIniciar)
+			{
+				$('#B_CargaProgramas').sfButton('blur');
+				$('#btnTest').sfButton('focus');
+				this.btnIniciar = false;
+			}
+
+			break;
+
+		case $.sfKey.ENTER:
+		
+			if(this.btnIniciar)
+			{
+				$.sfScene.hide('Scene1');
+				$.sfScene.show('Scene2');
+				$.sfScene.focus('Scene2');
+				
+				this.configuracion = false;
+			}
+			else
+			{
+				$.sfScene.hide('Scene1');
+				
+				$.sfScene.show('Scene6');
+				$.sfScene.focus('Scene6');
+				
+				this.configuracion = true;
+			}
+
+		break;
+		
+		case $.sfKey.RETURN:
+			$.sf.exit(false);
+		break;
+	}
+}
+
+//"http://75.126.62.178/~jemac/json.txt"
